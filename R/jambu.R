170699dffa81921723a8bf1b3a6b447621ac9450
JNovoletti <joao.novoletti@gmail.com>
2025-03-10 14:30:56 -0300

    update datasets

---
+++ R/jambu.R
@@ -0,0 +1,14 @@
+#' Jambu Dataset
+#'
+#' This dataset contains information on nematode population density (`p_i`) and crop yield (`y`).
+#' It is used in the `seinfitR` package for modeling plant growth in relation to nematode populations.
+#'
+#' @format A data frame with 5,002 rows and 2 columns:
+#' \describe{
+#'   \item{p_i}{Nematode population density (initial population)}
+#'   \item{y}{Crop yield, other plant growth parameter, or ratio of the estimated variable for plant growth in an initial population density of the nematode}
+#' }
+#' @usage data(jambu, package = "seinfitR")
+#' @source https://osf.io/pm94t
+#' @references Silva, M.F., Faccioli, F.C., Honório, A.P. et al. (2024). First report of angular leaf spot in *Acmella oleracea* caused by the foliar nematode *Aphelenchoides pseudobesseyi*. *J Plant Dis Prot*, 131, 1707–1720. https://doi.org/10.1007/s41348-024-00982-2
+"jambu"
